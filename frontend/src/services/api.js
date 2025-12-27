86808a4fa88a4e2e7a673cd4de34e10fff85f828
himanchalkaushale <himanchalkaushale432@gmail.com>
2025-12-27 15:15:16 +0530

    Frontend: Setup Axios API service

---
+++ frontend/src/services/api.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000/api';
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const repoService = {
+    syncRepo: async (owner, name) => {
+        const response = await api.post('/repositories/sync', { owner, name });
+        return response.data;
+    },
+
+    getRepoSignals: async (id) => {
+        const response = await api.get(`/repositories/${id}/signals`);
+        return response.data;
+    },
+
+    getRepoOverview: async (id) => {
+        const response = await api.get(`/repositories/${id}/overview`);
+        return response.data;
+    },
+
+    getContributorsHealth: async (id) => {
+        const response = await api.get(`/repositories/${id}/contributors-health`);
+        return response.data;
+    },
+
+    deleteRepo: async (id) => {
+        const response = await api.delete(`/repositories/${id}`);
+        return response.data;
+    },
+
+    getRepositories: async () => {
+        const response = await api.get('/repositories');
+        return response.data;
+    },
+
+    getPRBottlenecks: async (id) => {
+        const response = await api.get(`/repositories/${id}/pr-bottlenecks`);
+        return response.data;
+    },
+
+    getIssuesHealth: async (id) => {
+        const response = await api.get(`/repositories/${id}/issues-health`);
+        return response.data;
+    }
+};
+
+export default api;
